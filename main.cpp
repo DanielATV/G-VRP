63b3c970117e10e3644d35410da0d35f02662c75
Daniel Toro <daniel.torov@sansano.usm.cl>
2020-03-14 11:14:06 -0300

    Lectura de parametros

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bits/stdc++.h> 
+#include <regex> 
 using namespace std;
 
 class Geeks 
@@ -19,14 +21,32 @@ class Geeks
 };
 
 int main(){
+    
+    // Vector of string to save tokens 
+    vector <string> tokens; 
+      
 
-   fstream newfile;
-   newfile.open("AB101.dat",ios::in); //open a file to perform read operation using file object
-   if (newfile.is_open()){   //checking whether the file is open
-      string tp;
-      while(getline(newfile, tp)){ //read data from file object and put it into string.
-         cout << tp << "\n"; //print the data of the string
-      }
-      newfile.close(); //close the file object.
-   }
+    fstream newfile;
+
+    regex spaces ("\\s+");
+
+    newfile.open("AB101.dat",ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()){   //checking whether the file is open
+        string tp;
+        string dummy;
+        string replace = " ";
+
+        
+
+        getline(newfile, tp);
+        dummy = regex_replace(tp,spaces,replace);
+        //tokens.push_back(tp);
+        cout << dummy << '\n'; 
+
+        while(getline(newfile, tp)){ //read data from file object and put it into string.
+ 
+            //cout << tp << "\n"; //print the data of the string
+        }
+        newfile.close(); //close the file object.
+    }
 }
