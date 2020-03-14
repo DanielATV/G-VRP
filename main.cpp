b1bf759c8558d1fe28b39f4456e9d269a390a1d0
Daniel Toro <daniel.torov@sansano.usm.cl>
2020-03-14 17:54:00 -0300

    Leer archivo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <bits/stdc++.h> 
-#include <regex> 
+#include <sstream>
+#include <cmath> 
+
 using namespace std;
 
 class Geeks 
@@ -20,28 +21,52 @@ class Geeks
     } 
 };
 
+static double haversine(double lat1, double lon1, 
+                        double lat2, double lon2) 
+    { 
+        // distance between latitudes 
+        // and longitudes 
+        double dLat = (lat2 - lat1) * 
+                      M_PI / 180.0; 
+        double dLon = (lon2 - lon1) *  
+                      M_PI / 180.0; 
+  
+        // convert to radians 
+        lat1 = (lat1) * M_PI / 180.0; 
+        lat2 = (lat2) * M_PI / 180.0; 
+  
+        // apply formulae 
+        double a = pow(sin(dLat / 2), 2) +  
+                   pow(sin(dLon / 2), 2) *  
+                   cos(lat1) * cos(lat2); 
+        double rad = 4182.44949; 
+        double c = 2 * asin(sqrt(a)); 
+        return rad * c; 
+    }
 int main(){
-    
-    // Vector of string to save tokens 
-    vector <string> tokens; 
-      
+
+    // Variables para calcular la distancia
+    double lat1; 
+    double lon1; 
+    double lat2; 
+    double lon2;
 
     fstream newfile;
-
-    regex spaces ("\\s+");
 
     newfile.open("AB101.dat",ios::in); //open a file to perform read operation using file object
     if (newfile.is_open()){   //checking whether the file is open
         string tp;
-        string dummy;
-        string replace = " ";
-
-        
 
         getline(newfile, tp);
-        dummy = regex_replace(tp,spaces,replace);
-        //tokens.push_back(tp);
-        cout << dummy << '\n'; 
+	
+        stringstream s(tp); // Used for breaking words 
+        string word; // to store individual words 
+    
+        
+        while (s >> word){
+            cout << word << "\n";
+        }
+        
 
         while(getline(newfile, tp)){ //read data from file object and put it into string.
  
